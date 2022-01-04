e6aecd3bf6eb9d7cfea769afc28c35383787d8ae
Tim Enders <enders.tim@gmail.com>
2022-01-04 10:37:22 -0600

    working generic roller, and starting to parse the character stats

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 use clap::Parser;
 use rand::Rng;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct Args {
-    /// Name of the person to greet
     #[clap(short, long)]
-    name: String,
+    dice: String,
     // #[clap(short = 'l', long)]
     // blargh: String,
 
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
+    // #[clap(short, long, default_value_t = 1)]
+    // count: u8,
 }
 
-fn d20() -> u8 {
+fn dx(dice: String) -> Vec<u8> {
+    // let split = dice.split("d");
+    let mut rolls: Vec<u8> = Vec::new();
+    let vec: Vec<&str> = dice.split("d").collect(); 
+    let n: u8 = vec[0].parse().unwrap();
+    let d: u8 = vec[1].parse().unwrap();
     let mut rng = rand::thread_rng();
-    return rng.gen_range(1..21);
+    for _ in 1..n+1 {
+        rolls.push(rng.gen_range(1..d+1));
+    }
+    return rolls;
 }
-
-
-
-
-
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    println!("Hello {}!", args.dice);
+    let result = dx(args.dice);
+    for i in &result {
+        println!("{}", i);
     }
-    let result = d20();
-    print!("The roll is {}", result);
 }
